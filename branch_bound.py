a53b09dabfc997092e0e5c48227f2b826e8130e9
rajaPamar <rajaparmar8541@gmail.com>
2017-08-10 23:40:16 +0530

    Added started code and helper functions

--- branch_bound.py
+++ branch_bound.py
@@ -12,6 +12,13 @@ def inplace_count(goal_state,init):
 init=[]
 
 nums=[1,7,3,5,'',4,6,2,8]
+
+def make_grid(nums,init):
+	for i in range(1,10):
+		if(i%3==0):
+			init.append(nums[i-3:i])
+
+
 def shuffle(init,nums):
 	period=5
 #shuffling!
@@ -21,18 +28,22 @@ def shuffle(init,nums):
 		nums[ran1],nums[ran2]=nums[ran2],nums[ran1]
 		period=period-1
 
+def is_valid(init,row,col):
+	try:
+		if(init[row][col]>0):
+			return True
+	except:
+		return False
+
+	
+
 goal_state=[[1,2,3],
 			[4,'',5],
 			[6,7,8]]
 
 
+make_grid(nums,init)
+
+def move():
 
 
-
-for i in range(1,10):
-	if(i%3==0):
-		init.append(nums[i-3:i])
-
-
-print(init)
-print(inplace_count(goal_state,init))
