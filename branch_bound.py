0506fa290fe72d1b2abf43ff95baf67751317638
rajaPamar <rajaparmar8541@gmail.com>
2017-08-10 23:38:03 +0530

    Added started code

---
+++ branch_bound.py
@@ -0,0 +1,38 @@
+import random
+
+def inplace_count(goal_state,init):
+	count=0
+	for i in range(0,3):
+		for j in range(0,3):
+			if(goal_state[i][j]==init[i][j] and goal_state[i][j]!=''):
+				count=count+1
+	return count
+
+
+init=[]
+
+nums=[1,7,3,5,'',4,6,2,8]
+def shuffle(init,nums):
+	period=5
+#shuffling!
+	while(period>0):
+		ran1=random.randint(0,100)%9
+		ran2=random.randint(0,100)%9
+		nums[ran1],nums[ran2]=nums[ran2],nums[ran1]
+		period=period-1
+
+goal_state=[[1,2,3],
+			[4,'',5],
+			[6,7,8]]
+
+
+
+
+
+for i in range(1,10):
+	if(i%3==0):
+		init.append(nums[i-3:i])
+
+
+print(init)
+print(inplace_count(goal_state,init))
